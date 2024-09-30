78ac372ed92b8e1bea6574b1364ec290f1f7901d
Mario Arroyo <mario.brenesarroyo@ucr.ac.cr>
2024-09-29 20:11:33 -0600

    1

--- MIPS/Practica_de_examen/problema7.asm
+++ MIPS/Practica_de_examen/problema7.asm
@@ -10,7 +10,15 @@ main:
     lw $a1, N                      # Cargar el número de elementos N en $a1
     jal find_min_max               # Llamar a la función find_min_max
 
-    # Aquí puedes realizar otras operaciones si lo deseas, pero el arreglo ya está almacenado.
+    # Imprimir el valor máximo
+    move $a0, $v0                  # Mover el valor máximo a $a0 para imprimir
+    li $v0, 1                      # Servicio para imprimir entero
+    syscall                        # Imprimir el valor máximo
+
+    # Imprimir el valor mínimo
+    move $a0, $v1                  # Mover el valor mínimo a $a0 para imprimir
+    li $v0, 1                      # Servicio para imprimir entero
+    syscall                        # Imprimir el valor mínimo
 
     # Finalizar el programa
     li $v0, 10                     # Código de servicio para terminar el programa
@@ -28,7 +36,7 @@ find_min_max:
     add $t7, $s1, $zero            # Inicializar $t7 con el primer elemento (máximo)
     add $t3, $s1, $zero            # Inicializar $t3 con el primer elemento (mínimo)
     addi $s2, $zero, 1             # Inicializar el índice i en 1
-    lw $t8, N                      # Cargar el número de elementos en $t8
+    lw $t8, N                       # Cargar el número de elementos en $t8
 
 loop:
     beq $s2, $t8, end_loop         # Si el índice i es igual a N, salir del bucle
@@ -48,16 +56,16 @@ check_min:
     add $t3, $t4, $zero            # Actualizar el mínimo
 
 next:
-    addi $s2, $s2, 1                # Incrementar el índice i
-    j loop                          # Repetir el bucle
+    addi $s2, $s2, 1               # Incrementar el índice i
+    j loop                         # Repetir el bucle
 
 end_loop:
-    add $v0, $t7, $zero             # Guardar el valor máximo en $v0
-    add $v1, $t3, $zero             # Guardar el valor mínimo en $v1
+    move $v0, $t7                  # Guardar el valor máximo en $v0
+    move $v1, $t3                  # Guardar el valor mínimo en $v1
 
-    lw $ra, 12($sp)                 # Restaurar el valor de $ra
-    lw $s0, 8($sp)                  # Restaurar el valor de $s0
-    lw $s1, 4($sp)                  # Restaurar el valor de $s1
-    lw $s2, 0($sp)                  # Restaurar el valor de $s2
-    addi $sp, $sp, 16               # Restaurar el puntero de pila
-    jr $ra                          # Retornar
+    lw $ra, 12($sp)                # Restaurar el valor de $ra
+    lw $s0, 8($sp)                 # Restaurar el valor de $s0
+    lw $s1, 4($sp)                 # Restaurar el valor de $s1
+    lw $s2, 0($sp)                 # Restaurar el valor de $s2
+    addi $sp, $sp, 16              # Restaurar el puntero de pila
+    jr $ra                         # Retornar
