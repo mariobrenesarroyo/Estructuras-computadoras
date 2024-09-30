d640b9d4c8c6ce829796c7be7c0a3295f6f951bd
Mario Arroyo <mario.brenesarroyo@ucr.ac.cr>
2024-09-29 19:24:48 -0600

    2

--- MIPS/Practica_de_examen/problema7.asm
+++ MIPS/Practica_de_examen/problema7.asm
@@ -26,38 +26,35 @@ find_min_max:
     add $s0, $a0, $zero            # $s0 = dirección base del array A
     add $s1, $a1, $zero            # $s1 = N (número de elementos)
     lw $t0, 0($s0)                 # Cargar el primer elemento del array en $t0
-    add $v0, $t0, $zero            # Inicializar $v0 con el primer elemento (máximo)
-    add $v1, $t0, $zero            # Inicializar $v1 con el primer elemento (mínimo)
+    add $t7, $t0, $zero            # Inicializar $t7 con el primer elemento (máximo)
+    add $t3, $t0, $zero            # Inicializar $t3 con el primer elemento (mínimo)
     addi $s2, $zero, 1             # Inicializar el índice en 1
 
 loop:
     beq $s2, $s1, end_loop         # Si el índice es igual a N, salir del bucle
     sll $t1, $s2, 2                # Calcular el desplazamiento: índice * 4
     add $t2, $s0, $t1              # Calcular la dirección de A[indice]
-    lw $t3, 0($t2)                 # Cargar A[indice] en $t3
+    lw $t4, 0($t2)                 # Cargar A[indice] en $t4
 
     # Comparar y actualizar el máximo
-    slt $t4, $v0, $t3              # Si $v0 < $t3, $t4 = 1
-    bne $t4, $zero, update_max     # Si $v0 < $t3, saltar a update_max
-    j check_min                    # Ir a check_min
-
-update_max:
-    add $v0, $t3, $zero            # Actualizar el máximo
+    slt $t5, $t7, $t4              # Si $t7 < $t4, $t5 = 1
+    beq $t5, $zero, check_min      # Si $t7 >= $t4, saltar a check_min
+    add $t7, $t4, $zero            # Actualizar el máximo
 
 check_min:
     # Comparar y actualizar el mínimo
-    slt $t5, $t3, $v1              # Si $t3 < $v1, $t5 = 1
-    bne $t5, $zero, update_min     # Si $t3 < $v1, saltar a update_min
+    slt $t6, $t4, $t3              # Si $t4 < $t3, $t6 = 1
+    beq $t6, $zero, next           # Si $t4 >= $t3, saltar a next
+    add $t3, $t4, $zero            # Actualizar el mínimo
 
 next:
     addi $s2, $s2, 1               # Incrementar el índice
     j loop                         # Repetir el bucle
 
-update_min:
-    add $v1, $t3, $zero            # Actualizar el mínimo
-    j next                         # Ir a next
-
 end_loop:
+    add $v0, $t7, $zero            # Guardar el valor máximo en $v0
+    add $v1, $t3, $zero            # Guardar el valor mínimo en $v1
+
     lw $ra, 12($sp)                # Restaurar el valor de $ra
     lw $s0, 8($sp)                 # Restaurar el valor de $s0
     lw $s1, 4($sp)                 # Restaurar el valor de $s1
