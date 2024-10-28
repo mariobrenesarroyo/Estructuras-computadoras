45e713378b7907cc2f976ce9824a5b0baa8a7958
Mario Arroyo <mario.brenesarroyo@ucr.ac.cr>
2024-10-28 15:44:49 -0600

    signos de puntuación

--- MIPS/tarea/tarea2-2.asm
+++ MIPS/tarea/tarea2-2.asm
@@ -5,13 +5,15 @@ consonantes: .asciiz "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
 buffer: .space 100
 MAYUSCULAS: .asciiz "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 minusculas: .asciiz "abcdefghijklmnñopqrstuvwxyz"
+signos: .asciiz "!¡?¿.:;"
 mensaje_resultados: .asciiz "Resultados:\n"
-mensaje_contador_palabras: .asciiz "Cantidad de palabras: \n"
-mensaje_contador_caracteres: .asciiz "Cantidad de caracteres (letras): \n"
-mensaje_contador_vocales: .asciiz "Cantidad de vocales: \n"
-mensaje_contador_consonantes: .asciiz "Cantidad de consonantes: \n"
-mensaje_contador_mayusculas: .asciiz "Cantidad de MAYUSCULAS: \n"
+mensaje_contador_palabras: .asciiz "Cantidad de palabras: "
+mensaje_contador_caracteres: .asciiz "Cantidad de caracteres (letras): "
+mensaje_contador_vocales: .asciiz "Cantidad de vocales: "
+mensaje_contador_consonantes: .asciiz "Cantidad de consonantes: "
+mensaje_contador_mayusculas: .asciiz "Cantidad de MAYUSCULAS: "
 mensaje_contador_minusculas: .asciiz "Cantidad de minusculas: "
+mensaje_contador_signos: .asciiz "Cantidad de signos de puntuación: "
 
 contador_palabras: .word 0
 contador_caracteres: .word 0
@@ -19,6 +21,7 @@ contador_vocales: .word 0
 contador_consonantes: .word 0
 contador_mayusculas: .word 0
 contador_minusculas: .word 0
+contador_signos: .word 0
 
 .text
 main:
@@ -39,12 +42,14 @@ main:
     la $s0, consonantes         # Dirección de 'consonantes'
     la $s1, MAYUSCULAS          # Dirección de 'MAYUSCULAS'
     la $s2, minusculas          # Dirección de 'minusculas'
+    la $s5, signos              # Dirección de 'signos'
     addi $t2, $zero, 0          # Contador de palabras
     addi $t4, $zero, 0          # Contador de vocales
     addi $t5, $zero, 0          # Contador de consonantes
     addi $t6, $zero, 0          # Indicador de palabra (0 = fuera de palabra, 1 = en palabra)
     addi $s3, $zero, 0          # Contador de mayúsculas
     addi $s4, $zero, 0          # Contador de minúsculas
+    addi $s6, $zero, 0          # Contador de signos
 
 bucle_conteo:
     lb $t7, 0($t0)              # Leer el siguiente carácter
@@ -73,13 +78,14 @@ verificar_consonante:
     la $s0, consonantes         # Dirección de 'consonantes'
 verificar_consonante_2:
     lb $t8, 0($s0)              # Cargar consonante
-    beq $t8, $zero, verificar_mayuscula
+    beq $t8, $zero, verificar_signo
     beq $t7, $t8, detectar_consonante
     addi $s0, $s0, 1
     j verificar_consonante_2
 
 detectar_consonante:
     addi $t5, $t5, 1            # Incrementar contador de consonantes
+    j verificar_mayuscula
 
 verificar_mayuscula:
     la $s1, MAYUSCULAS          # Dirección de 'MAYUSCULAS'
@@ -98,13 +104,26 @@ verificar_minuscula:
     la $s2, minusculas          # Dirección de 'minusculas'
 verificar_minuscula_2:
     lb $t8, 0($s2)              # Cargar minúscula
-    beq $t8, $zero, palabra_detectada
+    beq $t8, $zero, verificar_signo
     beq $t7, $t8, detectar_minuscula
     addi $s2, $s2, 1
     j verificar_minuscula_2
 
 detectar_minuscula:
     addi $s4, $s4, 1            # Incrementar contador de minúsculas
+    j palabra_detectada
+
+verificar_signo:
+    la $s5, signos              # Dirección de 'signos'
+verificar_signo_2:
+    lb $t8, 0($s5)              # Cargar signo
+    beq $t8, $zero, palabra_detectada
+    beq $t7, $t8, detectar_signo
+    addi $s5, $s5, 1
+    j verificar_signo_2
+
+detectar_signo:
+    addi $s6, $s6, 1            # Incrementar contador de signos
 
 palabra_detectada:
     beq $t6, $zero, nueva_palabra
@@ -130,6 +149,7 @@ fin_bucle:
     sw $t5, contador_consonantes
     sw $s3, contador_mayusculas
     sw $s4, contador_minusculas
+    sw $s6, contador_signos
 
     # Calcular el total de caracteres como suma de vocales y consonantes
     add $t3, $t4, $t5            # Total caracteres = vocales + consonantes
@@ -147,6 +167,10 @@ fin_bucle:
     addi $v0, $zero, 1
     lw $a0, contador_palabras
     syscall
+    # Salto de línea
+    addi $v0, $zero, 11
+    li $a0, 10
+    syscall
 
     # Imprimir contador de caracteres
     addi $v0, $zero, 4
@@ -155,39 +179,71 @@ fin_bucle:
     addi $v0, $zero, 1
     lw $a0, contador_caracteres
     syscall
+    # Salto de línea
+    addi $v0, $zero, 11
+    li $a0, 10
+    syscall
 
-    # Imprimir contador de vocales
+    # Contador de vocales
     addi $v0, $zero, 4
     la $a0, mensaje_contador_vocales
     syscall
     addi $v0, $zero, 1
     lw $a0, contador_vocales
     syscall
+    # Salto de línea
+    addi $v0, $zero, 11
+    li $a0, 10
+    syscall
 
-    # Imprimir contador de consonantes
+    # Contador de consonantes
     addi $v0, $zero, 4
     la $a0, mensaje_contador_consonantes
     syscall
     addi $v0, $zero, 1
     lw $a0, contador_consonantes
     syscall
+    # Salto de línea
+    addi $v0, $zero, 11
+    li $a0, 10
+    syscall
 
-    # Imprimir contador de mayúsculas
+    # Contador de mayúsculas
     addi $v0, $zero, 4
     la $a0, mensaje_contador_mayusculas
     syscall
     addi $v0, $zero, 1
     lw $a0, contador_mayusculas
     syscall
+    # Salto de línea
+    addi $v0, $zero, 11
+    li $a0, 10
+    syscall
 
-    # Imprimir contador de minúsculas
+    # Contador de minúsculas
     addi $v0, $zero, 4
     la $a0, mensaje_contador_minusculas
     syscall
     addi $v0, $zero, 1
     lw $a0, contador_minusculas
     syscall
+    # Salto de línea
+    addi $v0, $zero, 11
+    li $a0, 10
+    syscall
 
-    # Terminar el programa
-    li $v0, 10
+    # Contador de signos de puntuación
+    addi $v0, $zero, 4
+    la $a0, mensaje_contador_signos
+    syscall
+    addi $v0, $zero, 1
+    lw $a0, contador_signos
+    syscall
+    # Salto de línea
+    addi $v0, $zero, 11
+    li $a0, 10
+    syscall
+
+    # Terminar programa
+    addi $v0, $zero, 10
     syscall
