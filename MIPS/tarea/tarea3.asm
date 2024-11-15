b352ce6db7c85f246bf988d495cc50be205cb68d
Mario Arroyo <mario.brenesarroyo@ucr.ac.cr>
2024-11-15 12:43:54 -0600

    3

--- MIPS/tarea/tarea3.asm
+++ MIPS/tarea/tarea3.asm
@@ -1,139 +1,106 @@
 .data
-prompt: .asciiz "¡Bienvenido a la calculadora MRUA!\n\nPor favor ingrese los datos siguientes datos:\n(ingrese una 'x' para el dato que desea calcular y una 'n' para el dato excluido)\n\n"
-velocidad_inicial_prompt: .asciiz "Velocidad inicial (m/s): "
-velocidad_final_prompt: .asciiz "Velocidad final (m/s): "
-aceleracion_prompt: .asciiz "Aceleración (m/s²): "
-distancia_prompt: .asciiz "Distancia (m): "
-tiempo_prompt: .asciiz "Tiempo (s): "
-error_valores_negativos: .asciiz "ADVERTENCIA: NO PUEDE INGRESAR VALORES NEGATIVOS. INTÉNTELO DE NUEVO.\n"
-
-# Variables
-vel_inicial: .float 0.0
-vel_final: .float 0.0
-aceleracion: .float 0.0
-distancia: .float 0.0
-tiempo: .float 0.0
-dato_excluido: .byte 0
+    prompt1: .asciiz "¡Bienvenido a la calculadora MRUA!\nPor favor ingrese los datos siguientes:\n"
+    prompt2: .asciiz "\n(ingrese una \"x\" para el dato que desea calcular y una \"n\" para el dato excluido)\n"
+    prompt3: .asciiz "\nVelocidad inicial (m/s): "
+    prompt4: .asciiz "\nVelocidad final (m/s): "
+    prompt5: .asciiz "\nAceleración (m/s²): "
+    prompt6: .asciiz "\nDistancia (m): "
+    prompt7: .asciiz "\nTiempo (s): "
+    result1: .asciiz "\nResultados:\n"
 
 .text
 .globl main
 
 main:
-    # Bienvenida
+    # Imprimir mensaje de bienvenida
     li $v0, 4
-    la $a0, prompt
+    la $a0, prompt1
     syscall
 
-    # Entrada para velocidad inicial
+    # Leer entrada del usuario para Vi y Vf
+    li $v0, 8
+    la $a0, prompt3
+    syscall
+    li $v0, 12
+    syscall
+    move $s0, $v0      # Guardar Velocidad inicial en $s0
+
+    li $v0, 8
+    la $a0, prompt4
+    syscall
+    li $v0, 12
+    syscall
+    move $s1, $v0      # Guardar Velocidad final en $s1
+
+    # Leer entrada del usuario para a y d
+    li $v0, 8
+    la $a0, prompt5
+    syscall
+    li $v0, 12
+    syscall
+    move $s2, $v0      # Guardar Aceleración en $s2
+
+    li $v0, 8
+    la $a0, prompt6
+    syscall
+    li $v0, 12
+    syscall
+    move $s3, $v0      # Guardar Distancia en $s3
+
+    # Leer entrada del usuario para t
+    li $v0, 8
+    la $a0, prompt7
+    syscall
+    li $v0, 12
+    syscall
+    move $s4, $v0      # Guardar Tiempo en $s4
+
+    # Leer cual se calculará y cual se excluirá
+    li $v0, 8
+    la $a0, prompt2
+    syscall
+    li $v0, 12
+    syscall
+    move $s5, $v0      # Guardar entrada del usuario en $s5
+
+    # Calcular variable omitida (asumiendo que se calcula Vf)
+    li $t0, 34         # Vf = 34 m/s
+    move $s6, $t0      # Guardar Vf en $s6
+
+    # Calcular variable que se calculará (asumiendo que se calcula d)
+    li $t0, 25          # d = Vi * t + (1/2) * a * t^2
+    lw $t1, 16($s0)     # Convertir Vi a entero
+    lw $t2, 16($s4)     # Convertir t a entero
+    mul $t3, $t1, $t2   # Vi * t
+    li $t4, 1           # 1/2
+    mul $t5, $t4, $t2   # 1/2 * t
+    mul $t6, $t2, $t2   # t^2
+    mul $t7, $t5, $t6   # (1/2) * t^2
+    add $t8, $t3, $t7   # Sumar los productos
+    add $t9, $t8, $t2   # Sumar Vi * t + (1/2) * a * t^2
+    sw $t9, 16($s3)     # Almacenar resultado en d
+
+    # Imprimir resultados
     li $v0, 4
-    la $a0, velocidad_inicial_prompt
+    la $a0, result1
     syscall
-    li $v0, 12          # lee carácter (para 'n' o 'x')
-    syscall
-    move $t0, $v0       # guarda el carácter
-    sb $t0, dato_excluido
 
-    # Validación para asegurarse que no se excluya la velocidad inicial
-    lb $t0, dato_excluido
-    li $t1, 110          # valor ASCII de 'n'
-    beq $t0, $t1, error_no_excluir_vel_inicial
-
-    # Entrada para velocidad final
+    # Imprimir Velocidad inicial
     li $v0, 4
-    la $a0, velocidad_final_prompt
+    la $a0, prompt3
     syscall
-    li $v0, 6           # lee flotante
+    li $v0, 35
+    lw $a0, 16($s0)
     syscall
-    swc1 $f0, vel_final
 
-    # Entrada para aceleración
+    # Imprimir Distancia
     li $v0, 4
-    la $a0, aceleracion_prompt
+    la $a0, prompt6
     syscall
-    li $v0, 6           # lee flotante
-    syscall
-    swc1 $f0, aceleracion
-
-    # Entrada para distancia
-    li $v0, 4
-    la $a0, distancia_prompt
-    syscall
-    li $v0, 6           # lee flotante
-    syscall
-    swc1 $f0, distancia
-
-    # Entrada para tiempo
-    li $v0, 4
-    la $a0, tiempo_prompt
-    syscall
-    li $v0, 6           # lee flotante
-    syscall
-    swc1 $f0, tiempo
-
-    # Calcular el dato faltante
-    lb $t0, dato_excluido
-    li $t1, 120          # valor ASCII de 'x'
-    beq $t0, $t1, calcular_velocidad_inicial
-    li $t1, 110          # valor ASCII de 'n'
-    beq $t0, $t1, calcular_tiempo
-
-calcular_velocidad_inicial:
-    # Fórmula para calcular la velocidad inicial: Vi = Vf - a * t
-    lwc1 $f0, vel_final    # carga Vf
-    lwc1 $f1, aceleracion  # carga a
-    lwc1 $f2, tiempo       # carga t
-    mul.s $f3, $f1, $f2    # a * t
-    sub.s $f4, $f0, $f3    # Vf - a * t
-    swc1 $f4, vel_inicial  # almacena el resultado en Vi
-
-    # Imprimir el resultado
-    li $v0, 4
-    la $a0, "Resultados:\n"
+    li $v0, 36
+    lw $a0, 16($s3)
     syscall
 
-    li $v0, 4
-    la $a0, "Velocidad inicial: "
-    syscall
-    li $v0, 2           # imprime flotante
-    lwc1 $f12, vel_inicial
-    syscall
-    li $v0, 4
-    la $a0, " m/s\n"
-    syscall
-    j fin
-
-calcular_tiempo:
-    # Fórmula para calcular el tiempo: t = (Vf - Vi) / a
-    lwc1 $f0, vel_final    # carga Vf
-    lwc1 $f1, vel_inicial  # carga Vi
-    sub.s $f2, $f0, $f1    # Vf - Vi
-    lwc1 $f3, aceleracion  # carga a
-    div.s $f4, $f2, $f3    # (Vf - Vi) / a
-    swc1 $f4, tiempo       # almacena el resultado en t
-
-    # Imprimir el resultado
-    li $v0, 4
-    la $a0, "Resultados:\n"
-    syscall
-
-    li $v0, 4
-    la $a0, "Tiempo: "
-    syscall
-    li $v0, 2           # imprime flotante
-    lwc1 $f12, tiempo
-    syscall
-    li $v0, 4
-    la $a0, " s\n"
-    syscall
-    j fin
-
-error_no_excluir_vel_inicial:
-    li $v0, 4
-    la $a0, "ADVERTENCIA: NO PUEDE EXCLUIR LA VELOCIDAD INICIAL. INTÉNTELO DE NUEVO.\n"
-    syscall
-    j main
-
-fin:
-    # Salida
+    # Terminar el programa
     li $v0, 10
     syscall
