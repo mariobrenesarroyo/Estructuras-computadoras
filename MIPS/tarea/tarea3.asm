f73c62d2adbb5cbb1f9aa6a60cf4dc8fc264dfcd
Mario Arroyo <mario.brenesarroyo@ucr.ac.cr>
2024-11-15 12:39:53 -0600

    3

--- MIPS/tarea/tarea3.asm
+++ MIPS/tarea/tarea3.asm
@@ -1,148 +1,139 @@
 .data
-    prompt1: .asciiz "¡Bienvenido a la calculadora MRUA!\n\n"
-    prompt2: .asciiz "Por favor ingrese los datos siguientes:\n"
-    prompt3: .asciiz "Velocidad inicial (m/s): "
-    prompt4: .asciiz "Velocidad final (m/s): "
-    prompt5: .asciiz "Aceleración (m/s²): "
-    prompt6: .asciiz "Distancia (m): "
-    prompt7: .asciiz "Tiempo (s): "
-    
-    # Variables para almacenar los datos ingresados por el usuario
-    Vi: .float 0.0
-    Vf: .float 0.0
-    a: .float 0.0
-    d: .float 0.0
-    t: .float 0.0
-    
-    # Variables para las opciones "x" y "n"
-    optionVi: .asciiz "n"
-    optionVf: .asciiz "n"
-    optionA: .asciiz "n"
-    optionD: .asciiz "n"
-    optionT: .asciiz "n"
+prompt: .asciiz "¡Bienvenido a la calculadora MRUA!\n\nPor favor ingrese los datos siguientes datos:\n(ingrese una 'x' para el dato que desea calcular y una 'n' para el dato excluido)\n\n"
+velocidad_inicial_prompt: .asciiz "Velocidad inicial (m/s): "
+velocidad_final_prompt: .asciiz "Velocidad final (m/s): "
+aceleracion_prompt: .asciiz "Aceleración (m/s²): "
+distancia_prompt: .asciiz "Distancia (m): "
+tiempo_prompt: .asciiz "Tiempo (s): "
+error_valores_negativos: .asciiz "ADVERTENCIA: NO PUEDE INGRESAR VALORES NEGATIVOS. INTÉNTELO DE NUEVO.\n"
 
-    tempInput: .asciiz "Temp input buffer\n"
+# Variables
+vel_inicial: .float 0.0
+vel_final: .float 0.0
+aceleracion: .float 0.0
+distancia: .float 0.0
+tiempo: .float 0.0
+dato_excluido: .byte 0
 
 .text
-    .globl main
+.globl main
 
 main:
-    # Imprimir bienvenida
+    # Bienvenida
     li $v0, 4
-    la $a0, prompt1
+    la $a0, prompt
+    syscall
+
+    # Entrada para velocidad inicial
+    li $v0, 4
+    la $a0, velocidad_inicial_prompt
+    syscall
+    li $v0, 12          # lee carácter (para 'n' o 'x')
+    syscall
+    move $t0, $v0       # guarda el carácter
+    sb $t0, dato_excluido
+
+    # Validación para asegurarse que no se excluya la velocidad inicial
+    lb $t0, dato_excluido
+    li $t1, 110          # valor ASCII de 'n'
+    beq $t0, $t1, error_no_excluir_vel_inicial
+
+    # Entrada para velocidad final
+    li $v0, 4
+    la $a0, velocidad_final_prompt
+    syscall
+    li $v0, 6           # lee flotante
+    syscall
+    swc1 $f0, vel_final
+
+    # Entrada para aceleración
+    li $v0, 4
+    la $a0, aceleracion_prompt
+    syscall
+    li $v0, 6           # lee flotante
+    syscall
+    swc1 $f0, aceleracion
+
+    # Entrada para distancia
+    li $v0, 4
+    la $a0, distancia_prompt
+    syscall
+    li $v0, 6           # lee flotante
+    syscall
+    swc1 $f0, distancia
+
+    # Entrada para tiempo
+    li $v0, 4
+    la $a0, tiempo_prompt
+    syscall
+    li $v0, 6           # lee flotante
+    syscall
+    swc1 $f0, tiempo
+
+    # Calcular el dato faltante
+    lb $t0, dato_excluido
+    li $t1, 120          # valor ASCII de 'x'
+    beq $t0, $t1, calcular_velocidad_inicial
+    li $t1, 110          # valor ASCII de 'n'
+    beq $t0, $t1, calcular_tiempo
+
+calcular_velocidad_inicial:
+    # Fórmula para calcular la velocidad inicial: Vi = Vf - a * t
+    lwc1 $f0, vel_final    # carga Vf
+    lwc1 $f1, aceleracion  # carga a
+    lwc1 $f2, tiempo       # carga t
+    mul.s $f3, $f1, $f2    # a * t
+    sub.s $f4, $f0, $f3    # Vf - a * t
+    swc1 $f4, vel_inicial  # almacena el resultado en Vi
+
+    # Imprimir el resultado
+    li $v0, 4
+    la $a0, "Resultados:\n"
     syscall
 
     li $v0, 4
-    la $a0, prompt2
+    la $a0, "Velocidad inicial: "
     syscall
-
-    # Leer la entrada para la Velocidad Inicial (Vi)
+    li $v0, 2           # imprime flotante
+    lwc1 $f12, vel_inicial
+    syscall
     li $v0, 4
-    la $a0, prompt3
+    la $a0, " m/s\n"
     syscall
+    j fin
 
-    li $v0, 8  # Syscall para leer una cadena de caracteres
-    la $a0, tempInput
-    li $a1, 20  # Leer hasta 20 caracteres
-    syscall
-    la $t0, optionVi   # Almacenar la opción de la Velocidad inicial
-    lb $t1, tempInput  # Leer la primera letra de la entrada
+calcular_tiempo:
+    # Fórmula para calcular el tiempo: t = (Vf - Vi) / a
+    lwc1 $f0, vel_final    # carga Vf
+    lwc1 $f1, vel_inicial  # carga Vi
+    sub.s $f2, $f0, $f1    # Vf - Vi
+    lwc1 $f3, aceleracion  # carga a
+    div.s $f4, $f2, $f3    # (Vf - Vi) / a
+    swc1 $f4, tiempo       # almacena el resultado en t
 
-    beq $t1, 120, skipVi  # Si la entrada es "x" (ASCII 120), saltar la lectura de la Velocidad inicial
-    beq $t1, 110, skipVi  # Si la entrada es "n" (ASCII 110), saltar la lectura de la Velocidad inicial
-
-    # Leer el valor de la Velocidad Inicial (si no es "x" ni "n")
-    li $v0, 6  # Syscall para leer un número de punto flotante
-    syscall
-    s.s $f0, Vi  # Guardar el valor de Vi
-
-skipVi:
-    # Leer la entrada para la Velocidad Final (Vf)
+    # Imprimir el resultado
     li $v0, 4
-    la $a0, prompt4
+    la $a0, "Resultados:\n"
     syscall
 
-    li $v0, 8  # Syscall para leer una cadena de caracteres
-    la $a0, tempInput
-    li $a1, 20  # Leer hasta 20 caracteres
-    syscall
-    la $t0, optionVf   # Almacenar la opción de la Velocidad final
-    lb $t1, tempInput  # Leer la primera letra de la entrada
-
-    beq $t1, 120, skipVf  # Si la entrada es "x" (ASCII 120), saltar la lectura de la Velocidad final
-    beq $t1, 110, skipVf  # Si la entrada es "n" (ASCII 110), saltar la lectura de la Velocidad final
-
-    # Leer el valor de la Velocidad Final (si no es "x" ni "n")
-    li $v0, 6  # Syscall para leer un número de punto flotante
-    syscall
-    s.s $f0, Vf  # Guardar el valor de Vf
-
-skipVf:
-    # Leer la entrada para la Aceleración (a)
     li $v0, 4
-    la $a0, prompt5
+    la $a0, "Tiempo: "
     syscall
-
-    li $v0, 8  # Syscall para leer una cadena de caracteres
-    la $a0, tempInput
-    li $a1, 20  # Leer hasta 20 caracteres
+    li $v0, 2           # imprime flotante
+    lwc1 $f12, tiempo
     syscall
-    la $t0, optionA   # Almacenar la opción de Aceleración
-    lb $t1, tempInput  # Leer la primera letra de la entrada
-
-    beq $t1, 120, skipA  # Si la entrada es "x" (ASCII 120), saltar la lectura de la Aceleración
-    beq $t1, 110, skipA  # Si la entrada es "n" (ASCII 110), saltar la lectura de la Aceleración
-
-    # Leer el valor de la Aceleración (si no es "x" ni "n")
-    li $v0, 6  # Syscall para leer un número de punto flotante
-    syscall
-    s.s $f0, a  # Guardar el valor de a
-
-skipA:
-    # Leer la entrada para la Distancia (d)
     li $v0, 4
-    la $a0, prompt6
+    la $a0, " s\n"
     syscall
+    j fin
 
-    li $v0, 8  # Syscall para leer una cadena de caracteres
-    la $a0, tempInput
-    li $a1, 20  # Leer hasta 20 caracteres
-    syscall
-    la $t0, optionD   # Almacenar la opción de Distancia
-    lb $t1, tempInput  # Leer la primera letra de la entrada
-
-    beq $t1, 120, skipD  # Si la entrada es "x" (ASCII 120), saltar la lectura de la Distancia
-    beq $t1, 110, skipD  # Si la entrada es "n" (ASCII 110), saltar la lectura de la Distancia
-
-    # Leer el valor de la Distancia (si no es "x" ni "n")
-    li $v0, 6  # Syscall para leer un número de punto flotante
-    syscall
-    s.s $f0, d  # Guardar el valor de d
-
-skipD:
-    # Leer la entrada para el Tiempo (t)
+error_no_excluir_vel_inicial:
     li $v0, 4
-    la $a0, prompt7
+    la $a0, "ADVERTENCIA: NO PUEDE EXCLUIR LA VELOCIDAD INICIAL. INTÉNTELO DE NUEVO.\n"
     syscall
+    j main
 
-    li $v0, 8  # Syscall para leer una cadena de caracteres
-    la $a0, tempInput
-    li $a1, 20  # Leer hasta 20 caracteres
-    syscall
-    la $t0, optionT   # Almacenar la opción de Tiempo
-    lb $t1, tempInput  # Leer la primera letra de la entrada
-
-    beq $t1, 120, skipT  # Si la entrada es "x" (ASCII 120), saltar la lectura del Tiempo
-    beq $t1, 110, skipT  # Si la entrada es "n" (ASCII 110), saltar la lectura del Tiempo
-
-    # Leer el valor de Tiempo (si no es "x" ni "n")
-    li $v0, 6  # Syscall para leer un número de punto flotante
-    syscall
-    s.s $f0, t  # Guardar el valor de t
-
-skipT:
-    # Aquí calcularemos el valor faltante según las opciones "x" y "n"
-
-    # Finalizar el programa
+fin:
+    # Salida
     li $v0, 10
     syscall
