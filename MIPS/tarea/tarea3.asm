08bbcea1e29dd27a479b5e60a01269b274c26412
Mario Arroyo <mario.brenesarroyo@ucr.ac.cr>
2024-11-15 10:40:50 -0600

    tarea 3 2

--- MIPS/tarea/tarea3.asm
+++ MIPS/tarea/tarea3.asm
@@ -1,97 +1,175 @@
 .data
-promptVi: .asciiz "Velocidad inicial (Vi) (m/s): "   # Mensaje para solicitar Vi
-promptVf: .asciiz "Velocidad final (Vf) (m/s): "     # Mensaje para solicitar Vf
-promptA: .asciiz "Aceleracion (a) (m/s^2): "         # Mensaje para solicitar a
-promptD: .asciiz "Distancia (d) (m): "               # Mensaje para solicitar d
-promptT: .asciiz "Tiempo (t) (s): "                  # Mensaje para solicitar t
-msgInvalid: .asciiz "Valor invalido, intente de nuevo.\n"  # Mensaje para valores inválidos
-msgResult: .asciiz "Resultado: "                     # Mensaje para mostrar resultado
-Vi: .word 0                                          # Variable para almacenar Vi
-Vf: .word 0                                          # Variable para almacenar Vf
-a: .word 0                                           # Variable para almacenar a
-d: .word 0                                           # Variable para almacenar d
-t: .word 0                                           # Variable para almacenar t
+promptVi: .asciiz "Velocidad inicial (m/s) o 'x' si desconocida: "
+promptVf: .asciiz "Velocidad final (m/s) o 'x' si desconocida: "
+promptA: .asciiz "Aceleracion (m/s^2) o 'x' si desconocida: "
+promptD: .asciiz "Distancia (m) o 'x' si desconocida: "
+promptT: .asciiz "Tiempo (s) o 'x' si desconocido: "
+msgVi: .asciiz "Velocidad inicial calculada: "
+msgVf: .asciiz "Velocidad final calculada: "
+msgA: .asciiz "Aceleracion calculada: "
+msgD: .asciiz "Distancia calculada: "
+msgT: .asciiz "Tiempo calculado: "
+buffer: .space 2
+Vi: .word 0
+Vf: .word 0
+a: .word 0
+d: .word 0
+t: .word 0
 
 .text
 main:
-    # Pedir Velocidad inicial (Vi)
-    la $a0, promptVi          # Cargar dirección del mensaje de Vi en $a0
-    li $v0, 4                 # Código de syscall para imprimir cadena
-    syscall                   # Llamar al sistema para imprimir
-    li $v0, 5                 # Código de syscall para leer entero
-    syscall                   # Llamar al sistema para leer
-    sw $v0, Vi                # Guardar el valor de Vi
+    # Leer Vi
+    la $a0, promptVi
+    li $v0, 4
+    syscall
+    li $v0, 8
+    la $a1, buffer
+    li $a2, 2
+    syscall
+    lb $t0, buffer
+    li $t1, 'x'
+    beq $t0, $t1, calcular_vi
+    li $v0, 5
+    syscall
+    sw $v0, Vi
 
-    # Pedir Velocidad final (Vf)
-    la $a0, promptVf          # Cargar dirección del mensaje de Vf en $a0
-    li $v0, 4                 # Código de syscall para imprimir cadena
-    syscall                   # Llamar al sistema para imprimir
-    li $v0, 5                 # Código de syscall para leer entero
-    syscall                   # Llamar al sistema para leer
-    sw $v0, Vf                # Guardar el valor de Vf
+    # Leer Vf
+    la $a0, promptVf
+    li $v0, 4
+    syscall
+    li $v0, 8
+    la $a1, buffer
+    li $a2, 2
+    syscall
+    lb $t0, buffer
+    beq $t0, $t1, calcular_vf
+    li $v0, 5
+    syscall
+    sw $v0, Vf
 
-    # Pedir Aceleracion (a)
-    la $a0, promptA           # Cargar dirección del mensaje de a en $a0
-    li $v0, 4                 # Código de syscall para imprimir cadena
-    syscall                   # Llamar al sistema para imprimir
-    li $v0, 5                 # Código de syscall para leer entero
-    syscall                   # Llamar al sistema para leer
-    sw $v0, a                 # Guardar el valor de a
+    # Leer a
+    la $a0, promptA
+    li $v0, 4
+    syscall
+    li $v0, 8
+    la $a1, buffer
+    li $a2, 2
+    syscall
+    lb $t0, buffer
+    beq $t0, $t1, calcular_a
+    li $v0, 5
+    syscall
+    sw $v0, a
 
-    # Pedir Distancia (d)
-    la $a0, promptD           # Cargar dirección del mensaje de d en $a0
-    li $v0, 4                 # Código de syscall para imprimir cadena
-    syscall                   # Llamar al sistema para imprimir
-    li $v0, 5                 # Código de syscall para leer entero
-    syscall                   # Llamar al sistema para leer
-    sw $v0, d                 # Guardar el valor de d
+    # Leer d
+    la $a0, promptD
+    li $v0, 4
+    syscall
+    li $v0, 8
+    la $a1, buffer
+    li $a2, 2
+    syscall
+    lb $t0, buffer
+    beq $t0, $t1, calcular_d
+    li $v0, 5
+    syscall
+    sw $v0, d
 
-    # Pedir Tiempo (t)
-    la $a0, promptT           # Cargar dirección del mensaje de t en $a0
-    li $v0, 4                 # Código de syscall para imprimir cadena
-    syscall                   # Llamar al sistema para imprimir
-    li $v0, 5                 # Código de syscall para leer entero
-    syscall                   # Llamar al sistema para leer
-    sw $v0, t                 # Guardar el valor de t
+    # Leer t
+    la $a0, promptT
+    li $v0, 4
+    syscall
+    li $v0, 8
+    la $a1, buffer
+    li $a2, 2
+    syscall
+    lb $t0, buffer
+    beq $t0, $t1, calcular_t
+    li $v0, 5
+    syscall
+    sw $v0, t
+    j end
 
-    # Validación de entradas (simple)
-    lw $t0, Vi                # Cargar Vi en $t0
-    blez $t0, invalid         # Si Vi <= 0, saltar a invalid
-    lw $t0, Vf                # Cargar Vf en $t0
-    blez $t0, invalid         # Si Vf <= 0, saltar a invalid
-    lw $t0, a                 # Cargar a en $t0
-    blez $t0, invalid         # Si a <= 0, saltar a invalid
-    lw $t0, d                 # Cargar d en $t0
-    blez $t0, invalid         # Si d <= 0, saltar a invalid
-    lw $t0, t                 # Cargar t en $t0
-    blez $t0, invalid         # Si t <= 0, saltar a invalid
+calcular_vi:
+    # Vi = Vf - a * t
+    lw $t1, Vf
+    lw $t2, a
+    lw $t3, t
+    mul $t2, $t2, $t3        # a * t
+    sub $t0, $t1, $t2         # Vi = Vf - a * t
+    la $a0, msgVi
+    li $v0, 4
+    syscall
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    j end
 
-    # Cálculo de distancia (simplificado a enteros)
-    lw $t1, Vi                # Cargar Vi en $t1
-    lw $t2, t                 # Cargar t en $t2
-    mul $t3, $t1, $t2         # t3 = Vi * t
-    lw $t1, a                 # Cargar a en $t1
-    mul $t4, $t2, $t2         # t4 = t^2
-    div $t4, $t4, 2           # t4 = t^2 / 2
-    mul $t4, $t4, $t1         # t4 = a * t^2 / 2
-    add $t3, $t3, $t4         # t3 = Vi * t + (a * t^2 / 2)
+calcular_vf:
+    # Vf = Vi + a * t
+    lw $t1, Vi
+    lw $t2, a
+    lw $t3, t
+    mul $t2, $t2, $t3        # a * t
+    add $t0, $t1, $t2        # Vf = Vi + a * t
+    la $a0, msgVf
+    li $v0, 4
+    syscall
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    j end
 
-    # Imprimir resultado
-    la $a0, msgResult         # Cargar dirección de mensaje de resultado
-    li $v0, 4                 # Código de syscall para imprimir cadena
-    syscall                   # Llamar al sistema para imprimir
-    move $a0, $t3             # Colocar el resultado en $a0 para imprimir
-    li $v0, 1                 # Código de syscall para imprimir entero
-    syscall                   # Llamar al sistema para imprimir el resultado
+calcular_a:
+    # a = (Vf - Vi) / t
+    lw $t1, Vf
+    lw $t2, Vi
+    sub $t1, $t1, $t2        # Vf - Vi
+    lw $t3, t
+    div $t1, $t3             # (Vf - Vi) / t
+    mflo $t0
+    la $a0, msgA
+    li $v0, 4
+    syscall
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    j end
 
-    j end                     # Saltar a fin
+calcular_d:
+    # d = Vi * t + (1/2) * a * t^2
+    lw $t1, Vi
+    lw $t2, t
+    mul $t1, $t1, $t2        # Vi * t
+    lw $t3, a
+    mul $t3, $t3, $t2        # a * t
+    mul $t3, $t3, $t2        # a * t^2
+    srl $t3, $t3, 1          # (1/2) * a * t^2
+    add $t0, $t1, $t3        # d = Vi * t + (1/2) * a * t^2
+    la $a0, msgD
+    li $v0, 4
+    syscall
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    j end
 
-invalid:
-    la $a0, msgInvalid        # Cargar dirección de mensaje de error
-    li $v0, 4                 # Código de syscall para imprimir cadena
-    syscall                   # Llamar al sistema para imprimir
-    j main                    # Volver al inicio para reiniciar
+calcular_t:
+    # t = (Vf - Vi) / a
+    lw $t1, Vf
+    lw $t2, Vi
+    sub $t1, $t1, $t2        # Vf - Vi
+    lw $t3, a
+    div $t1, $t3             # (Vf - Vi) / a
+    mflo $t0
+    la $a0, msgT
+    li $v0, 4
+    syscall
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    j end
 
 end:
-    li $v0, 10                # Código de syscall para salir del programa
-    syscall                   # Llamar al sistema para terminar
+    li $v0, 10
+    syscall
