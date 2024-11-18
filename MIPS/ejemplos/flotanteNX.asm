97f92a39edc2d46136de1af80d22815fd19cc1a8
Mario Arroyo <mario.brenesarroyo@ucr.ac.cr>
2024-11-18 14:50:51 -0600

    23

--- MIPS/ejemplos/flotanteNX.asm
+++ MIPS/ejemplos/flotanteNX.asm
@@ -23,19 +23,23 @@ main:
     # Llamar a la función de verificación
     jal verificar_entrada
 
-    # Mostrar mensaje antes de imprimir el flotante
-    la $a0, mensaje_four    # Carga la dirección del mensaje "El flotante ingresado es:"
-    addiu $v0,$zero,4            # Llamada al sistema para imprimir string
-    syscall
+    # Leer número flotante ingresado
+        li $v0, 6               # Llamada al sistema para leer un número flotante
+        syscall
+        mov.s $f12, $f0         # Mueve el número leído de $f0 a $f12 (preparación para impresión)
 
-    # Imprimir el número flotante ingresado
-    addiu $v0, $zero, 2     # Llamada al sistema para imprimir un número flotante
-    mov.s $f12, $f0         # Mover el valor flotante al registro $f12
-    syscall
+        # Mostrar mensaje antes de imprimir el flotante
+        la $a0, mensaje_four    # Carga la dirección del mensaje "El flotante ingresado es:"
+        addiu $v0, $zero, 4     # Llamada al sistema para imprimir string
+        syscall
 
-    # Terminar el programa
-    li $v0, 10              # Llamada al sistema para salir del programa
-    syscall
+        # Imprimir el número flotante ingresado
+        addiu $v0, $zero, 2     # Llamada al sistema para imprimir un número flotante
+        syscall
+
+        # Terminar el programa
+        li $v0, 10              # Llamada al sistema para salir del programa
+        syscall
 
 verificar_entrada:
     # Verificar si la entrada es 'n' o 'x'
@@ -58,10 +62,16 @@ es_n:
     la $a0, ms1             # Cargar dirección del mensaje "el valor ingresado es una n"
     addiu $v0,$zero,4            # Llamada al sistema para imprimir string
     syscall
-    jr $ra
+
+    # Terminar el programa
+    li $v0, 10              # Llamada al sistema para salir del programa
+    syscall
 
 es_x:
     la $a0, ms2             # Cargar dirección del mensaje "el valor ingresado es una x"
     addiu $v0,$zero,4            # Llamada al sistema para imprimir string
     syscall
-    jr $ra
+    
+    # Terminar el programa
+    li $v0, 10              # Llamada al sistema para salir del programa
+    syscall
